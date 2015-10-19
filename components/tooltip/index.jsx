1882991175ea717ecaf758b87ced68b36ed924dd
afc163 <afc163@gmail.com>
2015-10-19 18:07:40 +0800

    first commit

---
+++ components/tooltip/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Tooltip from 'rc-tooltip';
+
+export default React.createClass({
+  getDefaultProps() {
+    return {
+      prefixCls: 'ant-tooltip',
+      placement: 'top',
+      mouseEnterDelay: 0.1,
+      mouseLeaveDelay: 0.1
+    };
+  },
+  render() {
+    let transitionName = ({
+      top: 'zoom-down',
+      bottom: 'zoom-up',
+      left: 'zoom-right',
+      right: 'zoom-left'
+    })[this.props.placement];
+    return (
+      <Tooltip transitionName={transitionName}
+        overlay={this.props.title}
+        {...this.props}>
+        {this.props.children}
+      </Tooltip>
+    );
+  }
+});
